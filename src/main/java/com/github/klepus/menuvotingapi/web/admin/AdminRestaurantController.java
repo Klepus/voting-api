424209dfd1ed0c906dc44c8cf688fc8c016d6469
Aleks Klepinin <aleks.klepinin@gmail.com>
2021-04-28 15:33:38 +0300

    Add Admin Controller with tests

---
+++ src/main/java/com/github/klepus/menuvotingapi/web/admin/AdminRestaurantController.java
@@ -0,0 +1,147 @@
+package com.github.klepus.menuvotingapi.web.admin;
+
+import com.github.klepus.menuvotingapi.model.Dish;
+import com.github.klepus.menuvotingapi.model.Restaurant;
+import com.github.klepus.menuvotingapi.repository.*;
+import com.github.klepus.menuvotingapi.tos.*;
+import com.github.klepus.menuvotingapi.util.exception.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import static com.github.klepus.menuvotingapi.util.ToUtil.*;
+import static com.github.klepus.menuvotingapi.util.ValidationUtil.*;
+import static com.github.klepus.menuvotingapi.web.RestEndpoints.*;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class AdminRestaurantController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final RestaurantRepository restaurantRepository;
+    private final DishRepository dishRepository;
+
+    @Autowired
+    public AdminRestaurantController(RestaurantRepository restaurantRepository, DishRepository dishRepository) {
+        this.restaurantRepository = restaurantRepository;
+        this.dishRepository = dishRepository;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping(value = POST_ADMIN_CREATE_RESTAURANT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RestaurantTo> createRestaurant(@RequestBody @Valid RestaurantTo restaurantTo, BindingResult r) {
+        log.info("Create restaurant with name='{}'", restaurantTo.getName());
+        fieldValidation(r);
+
+        Restaurant restaurant = restaurantCreate(restaurantTo);
+        Restaurant created = restaurantRepository.save(restaurant);
+
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(POST_ADMIN_CREATE_RESTAURANT + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+
+        return ResponseEntity.created(uriOfNewResource).body(
+                restaurantToCreate(restaurant)
+        );
+    }
+
+    @Transactional
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping(value = PUT_UPDATE_RESTAURANT_INFO, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<RestaurantTo> updateRestaurant(@RequestBody @Valid RestaurantTo restaurantTo, @PathVariable int id, BindingResult r) {
+        log.info("Update restaurant with id={}", id);
+        fieldValidation(r);
+
+        Restaurant existRestaurant = restaurantRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Restaurant with id=" + id + " not found."));
+        if (restaurantTo.getName() != null) existRestaurant.setName(restaurantTo.getName());
+        if (restaurantTo.getAddress() != null) existRestaurant.setAddress(restaurantTo.getAddress());
+        if (restaurantTo.getTelephone() != null) existRestaurant.setTelephone(restaurantTo.getTelephone());
+
+        return ResponseEntity.ok(
+                restaurantToCreate(restaurantRepository.save(existRestaurant))
+        );
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping(value = DELETE_RESTAURANT)
+    public void deleteRestaurant(@PathVariable int id) {
+        log.info("Delete restaurant with id={}", id);
+        checkNotFoundWithId(restaurantRepository.delete(id) != 0, id);
+    }
+
+    @Transactional
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping(value = POST_ADMIN_CREATE_MENU, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<LocalDate, List<MenuTo>>> createMenu(@PathVariable Integer id, @RequestBody @Valid MenuTo menuTo, BindingResult r) {
+        log.info("Create menu for restaurant with id={}", id);
+        fieldValidation(r);
+
+        Optional<List<Dish>> dishesForToday = dishRepository.getDishesBetweenSingleRestaurant(LocalDate.now(), LocalDate.now(), id);
+        if (dishesForToday.isEmpty()) {
+            Restaurant restaurant = checkNotFoundWithId(restaurantRepository.getOne(id), id);
+            Set<Dish> result = dishesCreate(menuTo.getDishes(), restaurant);
+            List<Dish> dishes = dishRepository.saveAll(result);
+
+            return new ResponseEntity<>(allMenuTosCreate(dishes), HttpStatus.CREATED);
+        } else {
+            throw new ForbiddenException("Menu for today is persisted already. Save again not allowed. " +
+                    "You can update current menu, or delete and than persist new menu.");
+        }
+    }
+
+    @Transactional
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping(value = PUT_UPDATE_MENU, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<LocalDate, List<MenuTo>>> updateMenu(@PathVariable Integer id, @RequestBody @Valid MenuTo menuTo, BindingResult r) {
+        log.info("Update menu for restaurant with id={}", id);
+        fieldValidation(r);
+
+        Optional<Restaurant> restaurant = restaurantRepository.getRestaurantWithDishesForToday(id, LocalDate.now());
+
+        if (restaurant.isEmpty()) {
+            throw new NotFoundException("Update not allowed. Menu for today not found. You have to persist new menu.");
+        } else {
+            dishRepository.deleteAll(restaurant.get().getDishes());
+            Set<Dish> newMenu = dishesCreate(menuTo.getDishes(), restaurant.get());
+
+            return new ResponseEntity<>(allMenuTosCreate(dishRepository.saveAll(newMenu)), HttpStatus.OK);
+        }
+    }
+
+    @Transactional
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @DeleteMapping(value = DELETE_MENU)
+    public void deleteMenu(@PathVariable Integer id) {
+        log.info("Delete menu for restaurant with id={}", id);
+
+        if (!restaurantRepository.existsById(id)) {
+            throw new NotFoundException("Delete not allowed. Restaurant with id=" + id + " not found.");
+        }
+
+        Optional<Restaurant> restaurant = restaurantRepository.getRestaurantWithDishesForToday(id, LocalDate.now());
+        if (restaurant.isEmpty()) {
+            throw new NotFoundException("Delete not allowed. Menu not found.");
+        } else {
+            dishRepository.deleteAll(restaurant.get().getDishes());
+        }
+    }
+}
---
+++ src/test/java/com/github/klepus/menuvotingapi/web/admin/AdminRestaurantControllerExceptionTest.java
@@ -0,0 +1,167 @@
+package com.github.klepus.menuvotingapi.web.admin;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.klepus.menuvotingapi.repository.DishRepository;
+import com.github.klepus.menuvotingapi.repository.RestaurantRepository;
+import com.github.klepus.menuvotingapi.tos.MenuTo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static com.github.klepus.menuvotingapi.web.RestEndpoints.*;
+import static com.github.klepus.menuvotingapi.AllTestData.*;
+import static com.github.klepus.menuvotingapi.UserTestData.*;
+import static com.github.klepus.menuvotingapi.util.TestUtil.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdminRestaurantControllerExceptionTest {
+    @MockBean
+    private RestaurantRepository restaurantRepository;
+
+    @MockBean
+    private DishRepository dishRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void updateRestaurant_NFE() throws Exception {
+        when(restaurantRepository
+                .findById(100000))
+                .thenReturn(Optional.empty());
+
+        mockMvc.perform(put(PUT_UPDATE_RESTAURANT_INFO, "100000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN, ADMIN_PASSWORD))
+                .content(objectMapper.writeValueAsString(createRestaurantTo(UPD_RESTAURANT))))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+
+        verify(restaurantRepository, times(1)).findById(100000);
+        verify(restaurantRepository, times(0)).save(any());
+    }
+
+    @Test
+    public void deleteRestaurant_NFE() throws Exception {
+        when(restaurantRepository
+                .delete(100000))
+                .thenReturn(0);
+
+        mockMvc.perform(delete(DELETE_RESTAURANT, "100000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN, ADMIN_PASSWORD)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+
+        verify(restaurantRepository, times(1)).delete(100000);
+    }
+
+    @Test
+    public void createMenu_menuAlreadyExist() throws Exception {
+        when(dishRepository
+                .getDishesBetweenSingleRestaurant(any(), any(), any()))
+                .thenReturn(Optional.of(Arrays.asList(DISH_15_R1, DISH_16_R1)));
+
+        mockMvc.perform(post(POST_ADMIN_CREATE_MENU, "100000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN, ADMIN_PASSWORD))
+                .content(objectMapper.writeValueAsString(new MenuTo(null,null, null, createDishTosSet(Arrays.asList(NEW_DISH_25_R3, NEW_DISH_26_R3))))))
+                .andExpect(status().isForbidden())
+                .andDo(print());
+        verify(dishRepository, times(1)).getDishesBetweenSingleRestaurant(any(), any(), any());
+        verify(restaurantRepository, times(0)).getOne(any());
+        verify(dishRepository, times(0)).saveAll(any());
+    }
+
+    @Test
+    public void createMenu_NFE() throws Exception {
+        when(dishRepository
+                .getDishesBetweenSingleRestaurant(any(), any(), any()))
+                .thenReturn(Optional.empty());
+
+        when(restaurantRepository
+                .getOne(any()))
+                .thenReturn(null);
+
+        mockMvc.perform(post(POST_ADMIN_CREATE_MENU, "100000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN, ADMIN_PASSWORD))
+                .content(objectMapper.writeValueAsString(new MenuTo(null,null, null, createDishTosSet(Arrays.asList(NEW_DISH_25_R3, NEW_DISH_26_R3))))))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+        verify(dishRepository, times(1)).getDishesBetweenSingleRestaurant(any(), any(), any());
+        verify(restaurantRepository, times(1)).getOne(any());
+        verify(dishRepository, times(0)).saveAll(any());
+    }
+
+    @Test
+    public void updateMenu_NFE() throws Exception {
+        when(restaurantRepository
+                .getRestaurantWithDishesForToday(anyInt(), any()))
+                .thenReturn(Optional.empty());
+
+        mockMvc.perform(put(PUT_UPDATE_MENU, "2")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN, ADMIN_PASSWORD))
+                .content(objectMapper.writeValueAsString(new MenuTo(null,null, null, createDishTosSet(Arrays.asList(UPD_DISH_23_R2, UPD_DISH_24_R2))))))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+
+        verify(restaurantRepository, times(1)).getRestaurantWithDishesForToday(any(), any());
+        verify(dishRepository, times(0)).deleteAll(any());
+        verify(dishRepository, times(0)).saveAll(any());
+    }
+
+    @Test
+    public void deleteMenu_NFE() throws Exception {
+        when(restaurantRepository
+                .existsById(100000))
+                .thenReturn(false);
+
+        mockMvc.perform(delete(DELETE_MENU, "100000")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN, ADMIN_PASSWORD)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+
+        verify(restaurantRepository, times(1)).existsById(100000);
+        verify(restaurantRepository, times(0)).getRestaurantWithDishesForToday(any(), any());
+        verify(dishRepository, times(0)).deleteAll(any());
+    }
+
+    @Test
+    public void deleteMenu_NFE_2() throws Exception {
+        when(restaurantRepository
+                .existsById(1))
+                .thenReturn(true);
+
+        when(restaurantRepository
+                .getRestaurantWithDishesForToday(anyInt(), any()))
+                .thenReturn(Optional.empty());
+
+        mockMvc.perform(delete(DELETE_MENU, "1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(userHttpBasic(ADMIN, ADMIN_PASSWORD)))
+                .andExpect(status().isNotFound())
+                .andDo(print());
+
+        verify(restaurantRepository, times(1)).existsById(1);
+        verify(restaurantRepository, times(1)).getRestaurantWithDishesForToday(any(), any());
+        verify(dishRepository, times(0)).deleteAll(any());
+    }
+}
