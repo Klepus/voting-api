c6015b6d6eab571a42aba9ffc57c2abf93b4866d
Aleks Klepinin <aleks.klepinin@gmail.com>
2021-04-28 10:10:28 +0300

    Initial commit

---
+++ src/main/java/com/github/klepus/menuvotingapi/MenuVotingApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.klepus.menuvotingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuVotingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuVotingApiApplication.class, args);
+	}
+
+}
